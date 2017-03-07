0ffbd0ab3d4e009c4883529c1982c4246a12cc47
José Luis Cánovas Sánchez <joseluis.canovas@outlook.com>
2017-03-07 13:38:31 +0100

    Improve error codes for exit function

--- Code/util_sc/p2abc_iot_toolkit_include/smartcard_utils_interface/error_codes.h
+++ Code/util_sc/p2abc_iot_toolkit_include/smartcard_utils_interface/error_codes.h
@@ -5,15 +5,15 @@
 extern "C" {
 #endif
 
-#define ERROR_CANT_MALLOC                       101
-#define ERROR_SOCKET                            201
-#define ERROR_CANT_OPEN_FILE                    401
-#define ERROR_CANT_READ_FILE                    402
-#define ERROR_BAD_JSON_VALUE_LENGTH             501
-#define ERROR_BAD_JSON_ARRAY_LENGTH             502
-#define ERROR_APDU_TOO_SHORT                    601
-#define ERROR_APDU_TOO_LONG                     602
-#define ERROR_OPENSSL                           701
+#define ERROR_CANT_MALLOC                       11
+#define ERROR_SOCKET                            12
+#define ERROR_CANT_OPEN_FILE                    21
+#define ERROR_CANT_READ_FILE                    22
+#define ERROR_BAD_JSON_VALUE_LENGTH             23
+#define ERROR_BAD_JSON_ARRAY_LENGTH             24
+#define ERROR_APDU_TOO_SHORT                    31
+#define ERROR_APDU_TOO_LONG                     32
+#define ERROR_OPENSSL                           41
 
 #ifdef __cplusplus
 }
